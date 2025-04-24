796874aea203c70f9a96966fc0323f618c88804f
BuildTools <unconfigured@null.spigotmc.org>
2025-04-25 01:50:54 +0200

    setting up the whole project

--- src/main/java/me/flintaxe/flintaxesplanetmod/FlintaxesPlanetMod.java
+++ src/main/java/me/flintaxe/flintaxesplanetmod/FlintaxesPlanetMod.java
@@ -1,5 +1,7 @@
 package me.flintaxe.flintaxesplanetmod;
 
+import me.flintaxe.flintaxesplanetmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -39,6 +41,8 @@ public class FlintaxesPlanetMod {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -54,7 +58,9 @@ public class FlintaxesPlanetMod {
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.JEREMEJEVITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/me/flintaxe/flintaxesplanetmod/item/ModItems.java
@@ -0,0 +1,18 @@
+package me.flintaxe.flintaxesplanetmod.item;
+
+import me.flintaxe.flintaxesplanetmod.FlintaxesPlanetMod;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(FlintaxesPlanetMod.MOD_ID);
+
+    public static final DeferredItem<Item> JEREMEJEVITE = ITEMS.register("jeremejevite",
+            () -> new Item(new Item.Properties()));
+    // TODO: CHANGE THE jeremejevite TEXTURE TO POINT TOP RIGHT!!!
+    public static void register (IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
